5bf5eb5a73dbf0d09f0ba00c14845ea9510d3e8b
JossieHaven <jossiehaven@gmail.com>
2025-01-21 21:56:35 -0600

    committing changes for badge code - unable to add badge

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,10 +1,9 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {
-  if (license !== "none") {
-    return `![Github license](https://img.shields.io/badge/license-${license}-blue.svg)`;
-  } return "";
-}
+// function renderLicenseBadge(license) {
+//   let badge = '![Static Badge](https://img.shields.io/badge/:badgeContent)'
+//   badge.classList.add('badge');
+
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
